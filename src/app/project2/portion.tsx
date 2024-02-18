f054b78d6998a291daa5bbdb8c43a6250284d6be
Shahzaib <shahzaib1224@hotmail.com>
2024-02-18 16:19:23 +0500

    responsive

--- src/app/project2/portion.tsx
+++ src/app/project2/portion.tsx
@@ -7,7 +7,7 @@ import RightImage from "../../../public/assets/diaomod.avif";
 
 const Portion = () => {
   return (
-    <div className="md:h-[90vh] h-[120vh] w-full mt-5">
+    <div className="md:h-[90vh] h-[150vh] w-full mt-5">
       <div className="h-12 flex justify-between items-center px-14 py-8 ">
         <h1 className="text-2xl     font-bold">Case studies</h1>
         <div className="flex justify-center font-bold items-center gap-8">
@@ -15,7 +15,7 @@ const Portion = () => {
           <p className="text-2xl ">01</p>
         </div>
       </div>
-      <div className="flex flex-col md:flex-row justify-between items-center px-14 mt-10">
+      <div className="flex flex-col md:flex-row justify-between items-center px-4 md:px-14 mt-10">
         <div className="">
           <Image
             src={LeftImage}
