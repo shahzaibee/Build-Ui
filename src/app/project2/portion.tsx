fd441656852f1868282c3172676f112b49beccf5
Shahzaib <shahzaib1224@hotmail.com>
2024-02-25 17:49:07 +0500

    pictures ammendment

--- src/app/project2/portion.tsx
+++ src/app/project2/portion.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
 import React from "react";
 import { FiArrowRight } from "react-icons/fi";
+import pic1 from "../../../public/assets/pic1.avif";
+import pic2 from "../../../public/assets/pic2.avif";
 
 import LeftImage from "../../../public/assets/sketch.jpg";
 import RightImage from "../../../public/assets/diaomod.avif";
 
 const Portion = () => {
   return (
-    <div className="md:h-[90vh] h-[150vh] w-full mt-5">
+    <div className="md:h-[130vh] h-[150vh]  w-full mt-5">
       <div className="h-12 flex justify-between items-center px-14 py-8 ">
         <h1 className="text-2xl     font-bold">Case studies</h1>
         <div className="flex justify-center font-bold items-center gap-8">
@@ -18,12 +20,12 @@ const Portion = () => {
       <div className="flex flex-col md:flex-row justify-between items-center px-4 md:px-14 mt-10">
         <div className="">
           <Image
-            src={LeftImage}
+            src={pic1}
             alt=""
             priority
             width={800}
             height={800}
-            className="w-[550px] h-[350px] object-cover object-top rounded-md"
+            className="w-[550px] h-[350px] object-cover object-top-[20px] rounded-md md:h-[600px]"
           />
           <div>
             <div className="flex justify-between items-center gap-8 md:gap-0 md:px-4 mt-5">
@@ -35,13 +37,21 @@ const Portion = () => {
           </div>
         </div>
         <div>
-          <Image
+          {/* <Image
             src={RightImage}
             alt=""
             priority
             width={800}
             height={800}
             className="w-[550px] h-[400px] md:h-0 object-cover object-center rounded-md mt-5 md:mt-0"
+          /> */}
+          <Image
+            src={pic2}
+            alt=""
+            priority
+            width={800}
+            height={800}
+            className="w-[550px] h-[350px] object-cover object-top-[10px] rounded-md md:h-[600px]"
           />
           <div>
             <div className="flex justify-between items-center gap-8 md:gap-0 md:px-4 mt-5">
